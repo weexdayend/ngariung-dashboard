3b2473d983bf8a8c23a59a0031c65feae434007b
weexdayend <weexdayend@gmail.com>
2023-08-26 01:16:44 +0700

    fixing schedule

--- src/pages/management/class/schedule/index.tsx
+++ src/pages/management/class/schedule/index.tsx
@@ -14,6 +14,7 @@ type Props = {
 }
 
 function Index({ token, fitnessData, outletData }: Props) {
+  
   const [updated, setUpdated] = useState(false)
   const [datas, setDatas] = useState(fitnessData)
 
