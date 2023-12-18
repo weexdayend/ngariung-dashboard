d69a082bf5b80e1dd51270df07b3c05abb66db9b
Irvan Gerhana <weexdayend@gmail.com>
2023-12-18 09:40:32 +0700

    redeploy ngariung dashboard

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,12 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import cookie from 'cookie';
 
+// Import the cors middleware
+import Cors from 'cors';
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ['GET'], // Add other HTTP methods as needed
+});
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    if (req.method !== 'GET') {
-      return res.status(405).end(); // Method Not Allowed
-    }
+  // Wrap the handler in the cors middleware
+  await new Promise((resolve, reject) => {
+    cors(req, res, (result) => (result instanceof Error ? reject(result) : resolve(result)));
+  });
+
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
   
+  try {  
     const cookies = cookie.parse(req.headers.cookie || '')
   
     const token = cookies.token
